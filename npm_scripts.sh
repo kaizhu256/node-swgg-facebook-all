d356ac367865ab3b2479144626790807270f4bcd
kai zhu <kaizhu256@gmail.com>
2018-02-13 09:05:59 +0700

    npm-script

--- npm_scripts.sh
+++ npm_scripts.sh
@@ -2,7 +2,7 @@
 
 shNpmScriptApidocRawCreate() {(set -e
 # this function will create the raw apidoc
-    find tmp/developer.github.com/v3 -name index.html | \
+    find tmp/developer.facebook.com/v3 -name index.html | \
         sed -e 's|/index.html||' | \
         sort | sed -e 's|$|/index.html|' | \
         xargs -I @ -n 1 sh -c 'printf "\\n@\\n" && cat @' | \
@@ -14,16 +14,19 @@ shNpmScriptApidocRawFetch() {(set -e
 # this function will fetch the raw apidoc
     mkdir -p tmp
     cd tmp
-    rm -fr developer.github.com
-    mkdir -p developer.github.com/v3/guides
-    mkdir -p developer.github.com/v3/libraries
-    mkdir -p developer.github.com/v3/users/emails
-    mkdir -p developer.github.com/v3/users/followers
-    mkdir -p developer.github.com/v3/users/keys
-    mkdir -p developer.github.com/v3/users/gpg_keys
-    mkdir -p developer.github.com/v3/users/administration
-    mkdir -p developer.github.com/v3/users/blocking
-    wget -l 1 -np -nv -r https://developer.github.com/v3/index.html
+    rm -fr developer.facebook.com
+    #!! mkdir -p developer.facebook.com/v3/guides
+    #!! mkdir -p developer.facebook.com/v3/libraries
+    #!! mkdir -p developer.facebook.com/v3/users/emails
+    #!! mkdir -p developer.facebook.com/v3/users/followers
+    #!! mkdir -p developer.facebook.com/v3/users/keys
+    #!! mkdir -p developer.facebook.com/v3/users/gpg_keys
+    #!! mkdir -p developer.facebook.com/v3/users/administration
+    #!! mkdir -p developer.facebook.com/v3/users/blocking
+    wget \
+        -U "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.1234.123 Safari/537.36" \
+        -l 2 -m -np -nv \
+        https://developers.facebook.com/docs/graph-api/reference/
 )}
 
 shNpmScriptPostinstall() {
