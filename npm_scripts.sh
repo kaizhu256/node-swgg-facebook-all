48a498a0beeab09af5e407d4c8edeea20a37cbc4
kai zhu <kaizhu256@gmail.com>
2018-02-14 12:00:39 +0700

    update

--- npm_scripts.sh
+++ npm_scripts.sh
@@ -3,7 +3,7 @@
 shNpmScriptApidocRawCreate() {(set -e
 # this function will create the raw apidoc
     cd tmp/apidoc.raw
-    find developers.facebook.com -type f | \
+    find developers.facebook.com/docs/graph-api/reference -type f | \
         xargs -n 100 node -e '
 // <script>
 /*jslint
@@ -31,13 +31,11 @@ process.argv.slice(1).forEach(function (file1, file2) {
     local.fs.readFile(file1, "utf8", function (error, data) {
         local.assert(!error, error);
         if (file1 !== file2) {
-            console.error(file1, file2);
             local.fs.unlink(file1, local.onErrorDefault);
         }
-        data = data
-            .replace((/(<script\b[\S\s]*?<\/script>)/g), "")
-            .replace((/(<\/\w+>)/g), "$1\n")
-            .replace((/(<\/\w+>)\n{2,}/g), "$1\n");
+        data = "<div data-click-area=\"main\">" + (data
+            .split("<div data-click-area=\"main\">")[1] || "")
+            .split("<script>")[0];
         local.fs.mkdir(local.path.dirname(file2), function () {
             local.fs.writeFile(file2, data, local.onErrorDefault);
         });
